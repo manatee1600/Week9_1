01e156318f73680588fe9dbe594014b8b6a92d67
elephant_seal <mikan.ha.orange123@gmail.com>
2023-03-21 17:10:20 +0900

    DB接続エラー

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -3,10 +3,9 @@ DROP TABLE IF EXISTS names;
 CREATE TABLE names (
   id int unsigned AUTO_INCREMENT,
   name VARCHAR(10) NOT NULL,
-  address VARCHAR(10),
   PRIMARY KEY(id)
 );
 
-INSERT INTO names (id,name,address) VALUES (1,"koyama","tokyo");
-INSERT INTO names (id,name,address) VALUES (2,"tanaka","kanagawa");
-INSERT INTO names (id,name,address) VALUES (3,"suzuki","saitama");
+INSERT INTO names (id,name) VALUES (1,"koyama");
+INSERT INTO names (id,name) VALUES (2,"tanaka");
+INSERT INTO names (id,name) VALUES (3,"suzuki");
